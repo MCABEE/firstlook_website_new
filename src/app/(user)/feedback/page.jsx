b13bd1f041e92df3703b67b2173479733a10a973
Muhammed Arfaq R V <arfaq20@gmail.com>
2024-06-17 23:41:39 +0530

    animation added bugs fixed

--- src/app/(user)/feedback/page.jsx
+++ src/app/(user)/feedback/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 import axios from "axios";
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import toast, { Toaster } from "react-hot-toast";
 
 const Feedback = () => {
@@ -13,6 +13,8 @@ const Feedback = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [base64, setBase64] = useState('');
     const [preview, setPreview] = useState('');
+
+    const dropdownRef = useRef(null);
 
     const handleFileChange = (event) => {
         const file = event.target.files[0];
@@ -50,7 +52,7 @@ const Feedback = () => {
     };
 
     useEffect(() => {
-        const dropzone = document.getElementById('dropzone-file');
+        const dropzone = document.getElementById('dropzone-container');
         dropzone.addEventListener('dragover', handleDragOver);
         dropzone.addEventListener('drop', handleDrop);
 
@@ -58,20 +60,73 @@ const Feedback = () => {
             dropzone.removeEventListener('dragover', handleDragOver);
             dropzone.removeEventListener('drop', handleDrop);
         };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target) && !event.target.closest('#subject-container')) {
+                setIsOpen(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [dropdownRef]);
 
     const uploadFeedback = async () => {
         try {
-            await axios.post('https://testapi.firstlook.pro/data/general/saveFeedback', {
-                imgAfterCrop: `data:image/png;base64,${base64}`,
-                email,
-                name,
-                phone,
-                subject,
-                message
-            });
-            toast.success('Thank you for your feedback, we appreciate your input and will use it to improve our services.');
+
+            if (!email && !name && !phone && !subject && !message) {
+                toast.error('Please Fill The Form')
+                return
+            }
+
+            if (!email) {
+                toast.error('Enter Your Email')
+                return
+            }
+
+            if (!name) {
+                toast.error('Enter Your Name')
+                return
+            }
+
+            if (!phone) {
+                toast.error('Enter Your Phone')
+                return
+            }
+
+            if (!subject) {
+                toast.error('Enter Subject')
+                return
+            }
+
+            if (!message) {
+                toast.error('Enter Message')
+                return
+            }
+
+            else {
+                await axios.post('https://api.firstlook.pro/data/general/saveFeedback', {
+                    imgAfterCrop: `data:image/png;base64,${base64}`,
+                    email,
+                    name,
+                    phone,
+                    subject,
+                    message
+                }).then(() => {
+                    toast.success('Thank you for your feedback, we appreciate your input and will use it to improve our services.');
+                    setEmail("")
+                    setMessage("")
+                    setName("")
+                    setPhone("")
+                    setSubject("")
+                    setBase64("")
+                })
+            }
         } catch (error) {
             toast.error('Please try again later!');
         }
@@ -90,7 +145,7 @@ const Feedback = () => {
                             <p className="text-[12px] sm:text-lg">
                                 Your email address
                             </p>
-                            <input type="text" className="border border-gray-400 rounded-md mt-2 py-1.5 px-2 w-[100%] md:w-[50%]" value={email} onChange={(e) => setEmail(e.target.value)} />
+                            <input type="email" className="border border-gray-400 rounded-md mt-2 py-1.5 px-2 w-[100%] md:w-[50%]" value={email} onChange={(e) => setEmail(e.target.value)} />
                         </div>
                         <div className="mt-8">
                             <p className="text-[12px] sm:text-lg">
@@ -102,14 +157,14 @@ const Feedback = () => {
                             <p className="text-[12px] sm:text-lg">
                                 Registered phone number
                             </p>
-                            <input type="text" className="border border-gray-400 rounded-md mt-2 py-1.5 px-2 w-[100%] md:w-[50%]" value={phone} onChange={(e) => setPhone(e.target.value)} />
+                            <input type="number" className="border border-gray-400 rounded-md mt-2 py-1.5 px-2 w-[100%] md:w-[50%]" value={phone} onChange={(e) => setPhone(e.target.value)} />
                         </div>
                         <div className="mt-8">
                             <p className="text-[12px] sm:text-lg">
                                 I want to communicate you about
                             </p>
-                            <div onClick={() => setIsOpen(!isOpen)} className="flex cursor-pointer">
-                                <input type="text" placeholder="Select your reason" className="border cursor-pointer border-gray-400 rounded-md mt-2 py-1.5 px-2 w-[100%] md:w-[50%]" value={subject} />
+                            <div id="subject-container" onClick={() => setIsOpen(!isOpen)} className="flex cursor-pointer">
+                                <input type="text" placeholder="Select your reason" className="border cursor-pointer border-gray-400 rounded-md mt-2 py-1.5 px-2 w-[100%] md:w-[50%]" value={subject} readOnly />
                                 <div className="-ml-7 mt-5">
                                     <svg width="15" height="10" viewBox="0 0 21 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M10.0814 10C9.26724 10 8.45304 9.68595 7.83658 9.06949L0.252982 1.4859C-0.0843272 1.14859 -0.0843272 0.590291 0.252982 0.252982C0.590291 -0.0843272 1.1486 -0.0843272 1.4859 0.252982L9.06951 7.83657C9.62781 8.39487 10.5351 8.39487 11.0934 7.83657L18.677 0.252982C19.0143 -0.0843272 19.5726 -0.0843272 19.9099 0.252982C20.2472 0.590291 20.2472 1.14859 19.9099 1.4859L12.3263 9.06949C11.7098 9.68595 10.8956 10 10.0814 10Z" fill="#444444" />
@@ -119,40 +174,40 @@ const Feedback = () => {
                         </div>
                         {
                             isOpen &&
-                                <div className='bg-white border border-[#444444] rounded-xl mt-4 p-4 w-[100%] md:w-[50%]'>
-                                    <div className='flex flex-col text-[16px] font-light'>
-                                        <p onClick={() => {
-                                            setSubject("new account signup");
-                                            setIsOpen(false);
-                                        }} className='mt-2 cursor-pointer'>
-                                            new account signup
-                                        </p>
-                                        <p onClick={() => {
-                                            setSubject("technical support");
-                                            setIsOpen(false);
-                                        }} className='mt-2 cursor-pointer'>
-                                            technical support
-                                        </p>
-                                        <p onClick={() => {
-                                            setSubject("payment issues");
-                                            setIsOpen(false);
-                                        }} className='mt-2 cursor-pointer'>
-                                            payment issues
-                                        </p>
-                                        <p onClick={() => {
-                                            setSubject("feedback");
-                                            setIsOpen(false);
-                                        }} className='mt-2 cursor-pointer'>
-                                            feedback
-                                        </p>
-                                        <p onClick={() => {
-                                            setSubject("business relations");
-                                            setIsOpen(false);
-                                        }} className='mt-2 cursor-pointer mb-2'>
-                                            business relations
-                                        </p>
-                                    </div>
+                            <div ref={dropdownRef} className='bg-white border border-[#444444] rounded-xl mt-4 p-4 w-[100%] md:w-[50%]'>
+                                <div className='flex flex-col text-[16px] font-light'>
+                                    <p onClick={() => {
+                                        setSubject("new account signup");
+                                        setIsOpen(false);
+                                    }} className='mt-2 cursor-pointer'>
+                                        New account signup
+                                    </p>
+                                    <p onClick={() => {
+                                        setSubject("technical support");
+                                        setIsOpen(false);
+                                    }} className='mt-2 cursor-pointer'>
+                                        Technical support
+                                    </p>
+                                    <p onClick={() => {
+                                        setSubject("payment issues");
+                                        setIsOpen(false);
+                                    }} className='mt-2 cursor-pointer'>
+                                        Payment issues
+                                    </p>
+                                    <p onClick={() => {
+                                        setSubject("feedback");
+                                        setIsOpen(false);
+                                    }} className='mt-2 cursor-pointer'>
+                                        Feedback
+                                    </p>
+                                    <p onClick={() => {
+                                        setSubject("business relations");
+                                        setIsOpen(false);
+                                    }} className='mt-2 cursor-pointer mb-2'>
+                                        Business relations
+                                    </p>
                                 </div>
+                            </div>
                         }
                         <div className="mt-8">
                             <p className="text-[12px] sm:text-lg">
@@ -165,7 +220,7 @@ const Feedback = () => {
                                 Attachments if any (Optional)
                             </p>
                             <div className="flex items-center justify-center w-[100%] md:w-[50%] mt-2.5">
-                                <label htmlFor="dropzone-file" id="dropzone-file" className="flex flex-col items-center justify-center w-full h-36 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
+                                <label htmlFor="file-input" id="dropzone-container" className="flex flex-col items-center justify-center w-full h-36 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
                                     <div className="flex flex-col items-center justify-center pt-5 pb-6">
                                         {preview ?
                                             <Image src={preview} alt="feedback image" width={50} height={50} />
@@ -179,7 +234,7 @@ const Feedback = () => {
                                             </>
                                         }
                                     </div>
-                                    <input id="dropzone-file" type="file" accept="image/*" className="hidden" onChange={handleFileChange} />
+                                    <input id="file-input" type="file" accept="image/*" className="hidden" onChange={handleFileChange} />
                                 </label>
                             </div>
                         </div>
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,8 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "FirstLook",
-  description: "Connecting singles worldwide, effortlessly. Your reliable platform for matchmaking !",
+  title: "FirstLook: Connecting Singles Globally",
   icons: {
     icon: "/icon.png",
   },
