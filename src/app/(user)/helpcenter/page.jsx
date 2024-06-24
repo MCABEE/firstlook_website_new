f76493c68667b2ae144a91f8ceca3f5436770ce6
Muhammed Arfaq R V <arfaq20@gmail.com>
2024-06-24 16:42:04 +0530

    bug fixed

--- src/app/(user)/helpcenter/page.jsx
+++ src/app/(user)/helpcenter/page.jsx
@@ -76,7 +76,7 @@ const HelpCenter = () => {
                                     >
                                         <h2 className="text-[12px] sm:text-[24px] mt-2 font-sans">{data?.qns}</h2>
 
-                                        <span className="sm:block hidden">
+                                        <span className="sm:block hidden mt-2">
                                             {openFAQ === data?.id ? (
                                                 <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                     <rect width="20" height="20" fill="#444444" />
@@ -89,7 +89,7 @@ const HelpCenter = () => {
                                                 </svg>
                                             )}
                                         </span>
-                                        <span className="sm:hidden block">
+                                        <span className="sm:hidden block mt-2">
                                             {openFAQ === data?.id ? (
                                                 <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                     <rect width="12" height="12" fill="#444444" />
