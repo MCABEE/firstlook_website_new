ba5ae6b31d263bb6a3a2076472dd30f54d03210f
ThanoojKhan <thanu1495@gmail.com>
2024-08-13 19:17:44 +0530

    test

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -5,7 +5,7 @@ import logo from '../../public/asset/logo_3.png'
 const Preloader = () => {
   return (
     <div className="w-full h-full overflow-hidden m-0 p-0 flex justify-center absolute z-10 bg-[#FCF5EB] items-center">
-      <Image src={logo} alt='preloader_image' className='animate-pulse'/>
+      <Image src={logo} alt='preloader_image' className='animate-pulse' />
     </div>
   );
 };
